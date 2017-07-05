116783ffe471cbacab435684441d7d0b7ea19bb2
Iam-sunshine <wcy_sunshine@163.com>
2017-07-05 15:23:45 +0800

    模板2.0

--- resources/views/admin/layout/index.blade.php
+++ resources/views/admin/layout/index.blade.php
@@ -4,31 +4,31 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Amaze UI Admin index Examples</title>
+    <title>{{ Config::get('app.title') }}</title>
     <meta name="description" content="这是一个 index 页面">
     <meta name="keywords" content="index">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="renderer" content="webkit">
     <meta http-equiv="Cache-Control" content="no-siteapp" />
-    <link rel="icon" type="image/png" href="{{asset('admin/assets/i/favicon.png')}}">
-    <link rel="apple-touch-icon-precomposed" href="{{asset('admin/assets/i/app-icon72x72@2x.png')}}">
+    <link rel="icon" type="image/png" href="assets/i/favicon.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/i/app-icon72x72@2x.png">
     <meta name="apple-mobile-web-app-title" content="Amaze UI" />
-    <script src="{{asset('admin/assets/js/echarts.min.js')}}"></script>
-    <link rel="stylesheet" href="{{asset('admin/assets/css/amazeui.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('admin/assets/css/amazeui.datatables.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('admin/assets/css/app.css')}}">
-    <script src="{{asset('admin/assets/js/jquery.min.js')}}"></script>
+    <script src="assets/js/echarts.min.js"></script>
+    <link rel="stylesheet" href="assets/css/amazeui.min.css" />
+    <link rel="stylesheet" href="assets/css/amazeui.datatables.min.css" />
+    <link rel="stylesheet" href="assets/css/app.css">
+    <script src="assets/js/jquery.min.js"></script>
 
 </head>
 
 <body data-type="index">
-    <script src="{{asset('admin/assets/js/theme.js')}}"></script>
+    <script src="assets/js/theme.js"></script>
     <div class="am-g tpl-g">
         <!-- 头部 -->
         <header>
             <!-- logo -->
             <div class="am-fl tpl-header-logo">
-                <a href="javascript:;"><img src="{{asset('admin/assets/img/logo.png')}}" alt=""></a>
+                <a href="javascript:;"><img src="assets/img/logo.png" alt=""></a>
             </div>
             <!-- 右侧内容 -->
             <div class="tpl-header-fluid">
@@ -96,17 +96,65 @@
                 </li>
                 
                 <li class="sidebar-nav-link">
-                    <a href="chart.html">
-                        <i class="am-icon-bar-chart sidebar-nav-link-logo"></i> 栏目管理
-
+                    <a href="javascript:;" class="sidebar-nav-sub-title">
+                        <i class="am-icon-table sidebar-nav-link-logo"></i> 栏目管理
+                        <span class="am-icon-chevron-down am-fr am-margin-right-sm sidebar-nav-sub-ico"></span>
                     </a>
-                </li>
-                <li class="sidebar-nav-link">
-                    <a href="chart.html">
-                        <i class="am-icon-bar-chart sidebar-nav-link-logo"></i> 板块管理
+                    <ul class="sidebar-nav sidebar-nav-sub">
+                        <li class="sidebar-nav-link">
+                            <a href="table-list.html">
+                                <span class="am-icon-angle-right sidebar-nav-link-logo"></span> 添加栏目
+                            </a>
+                        </li>
 
-                    </a>
+                        <li class="sidebar-nav-link">
+                            <a href="table-list-img.html">
+                                <span class="am-icon-angle-right sidebar-nav-link-logo"></span> 栏目列表
+                            </a>
+                        </li>
+                    </ul>
                 </li>
+                
+                 <li class="sidebar-nav-link">
+                    <a href="javascript:;" class="sidebar-nav-sub-title">
+                        <i class="am-icon-table sidebar-nav-link-logo"></i> 板块管理
+                        <span class="am-icon-chevron-down am-fr am-margin-right-sm sidebar-nav-sub-ico"></span>
+                    </a>
+                    <ul class="sidebar-nav sidebar-nav-sub">
+                        <li class="sidebar-nav-link">
+                            <a href="table-list.html">
+                                <span class="am-icon-angle-right sidebar-nav-link-logo"></span> 添加板块
+                            </a>
+                        </li>
+
+                        <li class="sidebar-nav-link">
+                            <a href="table-list-img.html">
+                                <span class="am-icon-angle-right sidebar-nav-link-logo"></span> 板块列表
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                
+                 <li class="sidebar-nav-link">
+                    <a href="javascript:;" class="sidebar-nav-sub-title">
+                        <i class="am-icon-table sidebar-nav-link-logo"></i> 后台用户管理
+                        <span class="am-icon-chevron-down am-fr am-margin-right-sm sidebar-nav-sub-ico"></span>
+                    </a>
+                    <ul class="sidebar-nav sidebar-nav-sub">
+                        <li class="sidebar-nav-link">
+                            <a href="table-list.html">
+                                <span class="am-icon-angle-right sidebar-nav-link-logo"></span> 管理员添加
+                            </a>
+                        </li>
+
+                        <li class="sidebar-nav-link">
+                            <a href="table-list-img.html">
+                                <span class="am-icon-angle-right sidebar-nav-link-logo"></span> 管理员列表
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+              
                 <li class="sidebar-nav-link">
                     <a href="chart.html">
                         <i class="am-icon-bar-chart sidebar-nav-link-logo"></i> 帖子管理
@@ -126,13 +174,7 @@
                     </a>
                 </li>
 
-                 <li class="sidebar-nav-link">
-                    <a href="chart.html">
-                        <i class="am-icon-bar-chart sidebar-nav-link-logo"></i> 后台管理员管理
-
-                    </a>
-                </li>
-
+               
                  <li class="sidebar-nav-link">
                     <a href="chart.html">
                         <i class="am-icon-bar-chart sidebar-nav-link-logo"></i> 网站信息管理
@@ -140,7 +182,31 @@
                     </a>
                 </li>
 
-              
+                 <li class="sidebar-nav-link">
+                    <a href="javascript:;" class="sidebar-nav-sub-title">
+                        <i class="am-icon-table sidebar-nav-link-logo"></i> 申请信息管理
+                        <span class="am-icon-chevron-down am-fr am-margin-right-sm sidebar-nav-sub-ico"></span>
+                    </a>
+                    <ul class="sidebar-nav sidebar-nav-sub">
+                        <li class="sidebar-nav-link">
+                            <a href="table-list.html">
+                                <span class="am-icon-angle-right sidebar-nav-link-logo"></span> 版主申请
+                            </a>
+                        </li>
+
+                        <li class="sidebar-nav-link">
+                            <a href="table-list-img.html">
+                                <span class="am-icon-angle-right sidebar-nav-link-logo"></span> 广告申请
+                            </a>
+                        </li>
+
+                        <li class="sidebar-nav-link">
+                            <a href="table-list-img.html">
+                                <span class="am-icon-angle-right sidebar-nav-link-logo"></span> 友情链接申请
+                            </a>
+                        </li>
+                    </ul>
+                </li>
 
 
              
@@ -159,10 +225,10 @@
         </div>
     </div>
     </div>
-    <script src="{{asset('admin/assets/js/amazeui.min.js')}}"></script>
-    <script src="{{asset('admin/assets/js/amazeui.datatables.min.js')}}"></script>
-    <script src="{{asset('admin/assets/js/dataTables.responsive.min.js')}}"></script>
-    <script src="{{asset('admin/assets/js/app.js')}}"></script>
+    <script src="assets/js/amazeui.min.js"></script>
+    <script src="assets/js/amazeui.datatables.min.js"></script>
+    <script src="assets/js/dataTables.responsive.min.js"></script>
+    <script src="assets/js/app.js"></script>
 
 </body>
 
