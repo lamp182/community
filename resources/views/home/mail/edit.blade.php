f5f875ed5f3ad8bc41a83a990d033a0125774556
chunlongwang <xlchunlong@163.com>
2017-07-17 15:40:18 +0800

    合并登录注册3

--- resources/views/home/mail/edit.blade.php
+++ resources/views/home/mail/edit.blade.php
@@ -5,6 +5,10 @@
 	<title>Document</title>
 </head>
 <body>
-	恭喜您验证成功：<h3>{{ $email }}</h3>,点击<a href="http://www.lege182.com/home/default/yanzheng?id={{$id}}&token={{$token}}">修改</a>您好!感谢您再次来到17173游戏论坛。
+	恭喜您验证成功：<h3>{{ $email }}</h3>
+	,点击
+	<a href="{{ url("home/default/yanzheng?id=".$id."&token=".$token) }}">
+	<!-- <a href="{{ url('home/default/yanzheng?id='.$id.'&token='.$token) }}"> -->
+	修改</a>您好!感谢您再次来到17173游戏论坛。
 </body>
 </html>
--- resources/views/home/mail/index.blade.php
+++ resources/views/home/mail/index.blade.php
@@ -5,6 +5,6 @@
 	<title>Document</title>
 </head>
 <body>
-	恭喜您注册成功：<h3>{{ $email }}</h3>,点击<a href="http://www.lege182.com/home/zhuce/jihuo?id={{$id}}&token={{$token}}">激活</a>您好!欢迎来到17173游戏论坛。
+	恭喜您注册成功：<h3>{{ $email }}</h3>,点击<a href="{{ url("home/zhuce/jihuo?id=".$id."&token=".$token) }}">激活</a>您好!欢迎来到17173游戏论坛。
 </body>
 </html>
