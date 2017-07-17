65468a11f870af370e1f9f760458d961e4a730ec
chunlongwang <xlchunlong@163.com>
2017-07-17 14:49:19 +0800

    解决错误

--- app/Http/Model/Userdetail.php
+++ app/Http/Model/Userdetail.php
@@ -4,22 +4,11 @@ namespace App\Http\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Userdetail extends Model
+class UserDetail extends Model
 {
+    //
     protected $table = 'userdetails';
     protected $primaryKey="id";
-
-    // protected $fillable = ['user_name', 'user_pass'];
-   
-    //限制哪些字段不能添加
     protected $guarded = [];
-
-    //关闭自动维护字段
     public $timestamps = false;
-
-    public function user()
-    {
-        return $this->belongsTo('App\Http\Model\User');
-    }
-
 }
